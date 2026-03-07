70bb5f60b215d9ed9a0d2c2bf086955f6fd85948
blessme247 <solabiabraham@gmail.com>
2026-03-07 19:17:41 +0100

    feat: add modifier to withdraw function in FundMe contract

--- FundMe.sol
+++ FundMe.sol
@@ -14,6 +14,12 @@ contract FundMe {
      // We can use a mapping
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         // Allow users to send $
         // Have a minimum $ sent
@@ -22,6 +28,31 @@ contract FundMe {
        
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+            payable(funder).transfer(address(this).balance);
+        }
+        // reset the array
+        funders = new address[](0);
+
+        // to send blockchain currencies, there are 3 ways
+        // 1. transfer()
+        payable(msg.sender).transfer(address(this).balance);
+        // 2. send()
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send Failed");
+        // 3. call()
+        (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success, "Call Failed");
+    }
+
+    modifier onlyOwner{
+        require(msg.sender == owner, "Must be owner!");
+        _;
     }
 
    
